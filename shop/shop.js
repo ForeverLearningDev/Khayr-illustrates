bfcdcc1e8ee02aad50a36cc11ad71d3eb5425b60
ForeverLearningDev <kareem9.3@icloud.com>
2024-05-29 10:01:22 +0100

    ID not recognised

--- shop/shop.js
+++ shop/shop.js
@@ -1,93 +1,83 @@
 let shop = document.getElementById('shop');
 
-var shopItemsData = [{
-    id:"firstId",
-    name:"Digital Illustration (One Person)",
-    price: 20.00,
-    desc: "Faceless illustration of one person",
-    img: ""
-},
-{
-    id:"secondId",
-    name:"Digital Illustration (Two People)",
-    price: 1.00,
-    desc: "Faceless illustration of two people",
-    img: ""
-},
-{
-    id:"thirdId",
-    name:"Digital Illustration (Three People +)",
-    price: 1.00,
-    desc: "Faceless illustration of three people or more",
-    img: ""
-},
-{
-    id:"fourthId",
-    name:"Acrylic Photo Block",
-    price: 5.00,
-    desc: "A printed photo in a minimalistic decor piece. (a clear, solid block)",
-    img: ""
-},
-{
-    id:"fifthId",
-    name:"A5 print",
-    price: 1.50,
-    desc: "A5 printed photo on photo printing paper",
-    img: ""
-},
-{
-    id:"sixthId",
-    name:"Framed",
-    price: 2.00,
-    desc: "Framed printed photo",
-    img: ""
-},
-{
-    id:"seventhId",
-    name:"Framed",
-    price: 2.00,
-    desc: "Framed printed photo",
-    img: ""
-},
-{
-    id:"eighthId",
-    name:"Framed",
-    price: 2.00,
-    desc: "Framed printed photo",
-    img: ""
-},
-];
+let basket = JSON.parse(localStorage.getItem("ShoppingData")) || [];
 
-var basket = [];
-var generateShop = () => {
-    return (shop.innerHTML= shopItemsData
-        .map((x) => {
-            var {id,name,price,desc,img} = x
-        return `
-        <div id=product-id-${id} class="item">
-                <img class="item-image" src=${img} alt="">
-                <div class="details">
-                    <h3>${name}</h3>
-                    <p>${desc}</p>
-                    <div class="price-quantity">
-                        <h2>£ ${price}</h2>
-                        <div class="buttons">
-                            <i onclick="decrement(${id})" class="bi bi-dash-circle"></i>
-                            <div id=${id} class="quantity">0</div>
-                            <i onclick="increment(${id})" class="bi bi-plus-circle"></i>
+let generateShop = () => {
+        return (shop.innerHTML= shopItemsData
+            .map((x) => {
+                let {id,name,price,desc,img} = x
+                let search = basket.find((x) => x.id === id) || [];
+            return `
+            <div id=product-id-${id} class="item">
+                    <img class="item-image" src=${img} alt="">
+                    <div class="details">
+                        <h3>${name}</h3>
+                        <p>${desc}</p>
+                        <div class="price-quantity">
+                            <h2 class="price">£ ${price.toFixed(2)}</h2>
+                            <div class="buttons">
+                                <i onclick="decrement(${id})" class="bi bi-dash-circle"></i>
+                                <div id=${id} class="quantity">${search.item === undefined? 0: search.item}</div>
+                                <i onclick="increment(${id})" class="bi bi-plus-circle"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        `
-    }).join(""));
+            `
+        }).join(""));
 };
 
 generateShop();
 
-var increment = (id) => {
-    var selectedItem = id;
-    var search = basket.find((x)=> x.id === selectedItem.id);
+
+let basketContainer = document.querySelector('.basket-items-container');
+function generateBasket () {
+    let total = document.querySelector('.total-value-wrap input');
+    if(basket.length !== 0){
+    return(basketContainer.innerHTML = basket.map((x) => {
+        let {id, item} = x
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+        <div class="basketItem">
+                    <img src="${search.img}">
+                    <h2>
+                        ${search.name}
+                    </h2>
+                    <label class="price">£ ${search.price.toFixed(2)}</label>
+                    <div class="quantity-container">
+                        <button onclick="decrement(${id})" class="decrement"> - </button>
+                        <div class="quantity">${item}</div>
+                        <button onclick="increment(${id})" class="increment"> + </button>
+                        <i onclick="removeItem(${id})" id=remove-basket-item class="bi bi-x"></i>
+                    </div>
+                </div>
+                `
+    }).join(''))
+    } else {
+        basketContainer.innerHTML = `
+        <h2 style="position: absolute; top: 50%; left: 30%;">BASKET IS EMPTY</h2>`;
+    }
+}
+
+generateBasket();
+
+    let closeButton = document.querySelector('.close-button');
+    let basketSummary = document.querySelector('.checkout-summary');
+    let basketIcon = document.querySelector('.cart');
+
+    basketIcon.addEventListener('click', function () {
+        closeButton.classList.remove('hidden');
+        basketSummary.classList.remove('hidden');
+    })
+
+    closeButton.addEventListener('click', function () {
+        closeButton.classList.add('hidden');
+        basketSummary.classList.add('hidden');
+    })
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
 
     if (search === undefined) {
         basket.push({
@@ -97,28 +87,43 @@ var increment = (id) => {
     } else{
         search.item +=1;
         }
-
     update(selectedItem.id);
+    localStorage.setItem("ShoppingData", JSON.stringify(basket));
 };
-var decrement = (id) => {
-    var selectedItem = id;
-    var search = basket.find((x)=> x.id === selectedItem.id);
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
 
-    if (search.item === 0)  return;
+    if(search === undefined) return;
+    else if (search.item === 0)  return;
     else{
         search.item -=1;
         }
-
     update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    localStorage.setItem("ShoppingData", JSON.stringify(basket));
 };
-var update = (id) => {
-    var search = basket.find((x)=> x.id === id);
-    //console.log(search.item);
+let update = (id) => {
+    let search = basket.find((x)=> x.id === id);
     document.getElementById(id).innerHTML = search.item;
     calculation();
 };
 
-var calculation = () => {
-    var cartIcon = document.getElementById("cartAmount");
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
     cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
 };
+
+calculation();
+
+let cartAmount = document.getElementsByClassName('cartAmount');
+if (cartAmount < 0) {
+    cartAmount.classList.add('empty');
+}
+
+function removeItem (id) {
+    let selectedItem = id;
+    basket = basket.filter((x) => x.id !== selectedItem.id);
+    localStorage.setItem("ShoppingData", JSON.stringify(basket));
+}
+
---
+++ shopItemsData.js
@@ -0,0 +1,57 @@
+let shopItemsData = [{
+    id:"Dig1",
+    name:"Digital Illustration (One Person)",
+    price: 20.00,
+    desc: "Faceless illustration of one person",
+    img: "/images/illustration1.jpg"
+},
+{
+    id:"Dig2",
+    name:"Digital Illustration (Two People)",
+    price: 1.00,
+    desc: "Faceless illustration of two people",
+    img: "/images/illustration2.jpg"
+},
+{
+    id:"Dig3",
+    name:"Digital Illustration (Three People +)",
+    price: 1.00,
+    desc: "Faceless illustration of three people or more",
+    img: "/images/illustration3.jpg"
+},
+{
+    id:"Pblk",
+    name:"Acrylic Photo Block",
+    price: 5.00,
+    desc: "A printed photo in a minimalistic decor piece. (a clear, solid block)",
+    img: "/images/illustration4.jpg"
+},
+{
+    id:"Prnt",
+    name:"A5 print",
+    price: 1.50,
+    desc: "A5 printed photo on photo printing paper",
+    img: "/images/illustration5.jpg"
+},
+{
+    id:"Frmd",
+    name:"Framed",
+    price: 2.00,
+    desc: "Framed printed photo",
+    img: "/images/illustration6.jpg"
+},
+{
+    id:"Haha",
+    name:"HaHa",
+    price: 2.00,
+    desc: "Framed printed photo",
+    img: "/images/illustration7.jpg"
+},
+{
+    id:"AhAh",
+    name:"AhAh",
+    price: 2.00,
+    desc: "Framed printed photo",
+    img: "/images/illustration8.jpg"
+},
+];
