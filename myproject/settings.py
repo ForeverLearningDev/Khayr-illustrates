38f2861d0fe3043ba3c6c56e836ead96333da3e2
ForeverLearningDev <130088217+ForeverLearningDev@users.noreply.github.com>
2024-09-17 19:33:04 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['khayrillustrates.com', 'www.khayrillustrates.com', '127.0.0.1']
+ALLOWED_HOSTS = ['khayrillustrates.com', 'www.khayrillustrates.com', '127.0.0.1', 'khayr-illustrates-production.up.railway.app']
 
 
 # Application definition
