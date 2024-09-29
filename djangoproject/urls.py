e292571155488580207579558e82c72586bd4347
ForeverLearningDev <130088217+ForeverLearningDev@users.noreply.github.com>
2024-09-29 22:33:23 +0100

    Update urls.py

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -20,7 +20,7 @@ from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('products/', views.products, name='products'),
